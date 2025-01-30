5c789b43ef29c45be80f905b4f0db7ba7c42ec1a
Yacine-213 <d.yacine2020@gmail.com>
2025-01-30 13:39:56 +0000

    p

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-number = 3.14159
-print("Float: {:.2f}".format(number)
+
+# 4-print_float.py
+number = 3.14159  # You can change this to any float
+print(f"Float: {number:.2f}")
+
