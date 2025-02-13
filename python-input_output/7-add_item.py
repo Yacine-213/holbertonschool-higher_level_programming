7415e8cbf0dd7c38387050d16da9daf99b7708ad
Yacine-213 <d.yacine2020@gmail.com>
2025-02-13 14:09:48 +0000

    i

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
+"""
+Script that add all argument to a Python list and save them to a JSON file.
+
+"""
+
+
+import sys
 import json
+from os.path import exists
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj):
-    """Saves an object to a JSON file."""
-    with open('add_item.json', 'w', encoding='utf-8') as f:
-        json.dump(my_obj, f)
+filename = "add_item.json"
 
+if exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
 
-def load_from_json_file(filename):
-    """Creates an object from a JSON file."""
-    with open(filename, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-    return data
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, filename)
+
