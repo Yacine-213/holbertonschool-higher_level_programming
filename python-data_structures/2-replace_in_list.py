cf29d95e3d4345f5509d3f20fbf44677db4984c8
Yacine-213 <d.yacine2020@gmail.com>
2025-02-16 11:59:45 +0000

    p

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list  # Return the original list if index is out of range
-    
-    my_list[idx] = element  # Replace the element at the given index
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
     return my_list
-
